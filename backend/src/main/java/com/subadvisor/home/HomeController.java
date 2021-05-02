a899a506bfe82514751f3ecdbca19ab809e68a8f
Matti Henning <s80116@beuth-hochschule.de>
2021-05-02 15:34:25 +0200

    MP-22 [REF] rename parentfolder from backend to home

--- backend/src/main/java/com/subadvisor/home/HomeController.java
+++ backend/src/main/java/com/subadvisor/home/HomeController.java
@@ -1,6 +1,5 @@
-package com.subadvisor.backend;
+package com.subadvisor.home;
 
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
