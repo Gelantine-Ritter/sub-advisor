a46943e7b931160ad87903e6f7ca4bd8f8ffa796
Matti Henning <s80116@beuth-hochschule.de>
2021-05-02 19:02:33 +0200

    MP-22 [ADD] new bash-script with different functions to start, stop and clean sub-advisor-docker-container and images on host-machine.

---
+++ scripts/docker-backend-helper.sh
@@ -0,0 +1,105 @@
+#!/bin/bash
+
+SUBADVISOR_IMAGE=subadvisor/backend
+SUBADVISOR_CONTAINER=subadvisor-backend
+
+CONTAINER_RUNNING=$(docker ps -q -f name=$SUBADVISOR_CONTAINER)
+CONTAINER_PRESENT=$(docker ps -aq -f name=$SUBADVISOR_CONTAINER)
+AMOUNT_CONTAINERS_PRESENT=0;
+AMOUNT_CONTAINERS_RUNNING=0;
+
+STEP=1
+SUB_COMMAND="[SUB_COMMAND] $STEP:"
+
+
+increase() {
+  ((STEP=$STEP+1))
+  SUB_COMMAND="[SUB_COMMAND] $STEP:"
+}
+
+count_containers_present() {
+  for c in $CONTAINER_PRESENT; do
+    ((AMOUNT_CONTAINERS_PRESENT=$AMOUNT_CONTAINERS_PRESENT+1))
+  done
+}
+
+count_containers_running() {
+  for c in $CONTAINER_RUNNING; do
+    ((AMOUNT_CONTAINERS_RUNNING=$AMOUNT_CONTAINERS_RUNNING+1))
+  done
+}
+
+docker_helper() {
+    count_containers_present
+    count_containers_running
+    case "$1" in
+      start) start; return ;;
+      clean) clean; return ;;
+      stop) stop; return ;;
+    esac
+}
+
+# docker-command-functions:
+
+start() {
+  if [ $CONTAINER_RUNNING ]
+  then
+    echo "$SUB_COMMAND there are already sub-advisor-container running.
+          If you have a problem with one of them run d-clean" && increase
+  elif [ $AMOUNT_CONTAINERS_PRESENT -gt  2 ]
+  then
+    echo "$SUB_COMMAND there are more then one stopped sub-advisor-container. Please remove unrequired containers
+          If you have a problem run d-clean or d-complete" && increase
+    exit
+  elif [ "$AMOUNT_CONTAINERS_PRESENT" -eq 0 ]
+  then
+    echo "$SUB_COMMAND there is no sub-advisor-container (stopped or running) present. Please run d-run or d-build." && increase
+  elif [ "$AMOUNT_CONTAINERS_PRESENT" -eq 1 ]
+  then
+    echo "$SUB_COMMAND start container:" && increase
+    docker start "$CONTAINER_PRESENT"
+  fi
+}
+
+stop(){
+  if [ $AMOUNT_CONTAINERS_RUNNING -eq 0 ]
+  then
+    echo "$SUB_COMMAND no sub-advisor-container running." && increase
+  else
+    echo "$SUB_COMMAND stop all sub-advisor-container" && increase
+    docker stop "$CONTAINER_RUNNING"
+  fi
+}
+
+clean() {
+    if [ $CONTAINER_PRESENT ]
+    then
+      if [ $CONTAINER_RUNNING ]
+      then
+        echo "$SUB_COMMAND stop all running container" && increase
+        docker stop "$CONTAINER_RUNNING"
+        echo "$SUB_COMMAND stopped all running container" && increase
+      else
+        echo "$SUB_COMMAND no running container" && increase
+      fi
+      echo "$SUB_COMMAND remove all present container" && increase
+      docker rm $CONTAINER_PRESENT
+      echo "$SUB_COMMAND removed all present container" && increase
+    else
+      echo "$SUB_COMMAND no sub-advisor-container present"  && increase
+    fi
+
+    if [ $IMAGE_PRESENT ]
+    then
+      echo "$SUB_COMMAND remove all images" && increase
+      docker rmi $IMAGE_PRESENT
+      echo "$SUB_COMMAND removed all images" && increase
+    else
+      echo "$SUB_COMMAND no sub-advisor-image present"  && increase
+    fi
+}
+
+
+
+
+
