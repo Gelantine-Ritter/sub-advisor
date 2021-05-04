23ebe8a3c924a8f1618a5605aafc8eb37960d894
Matti Henning <s80116@beuth-hochschule.de>
2021-05-04 22:36:12 +0200

    MP-69 [ADD] LoadDataBase-Configuration to log loading of mock-venues

---
+++ backend/src/main/java/com/subadvisor/LoadDatabase.java
@@ -0,0 +1,25 @@
+package com.subadvisor;
+
+import com.subadvisor.venue.Venue;
+import com.subadvisor.venue.VenueRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+class LoadDatabase {
+
+
+    private static final Logger log = LoggerFactory.getLogger(LoadDatabase.class);
+
+    @Bean
+    CommandLineRunner initDatabase(VenueRepository repository) {
+        return args -> {
+            log.info("Preloading " + repository.save(new Venue("://about_blank", "blank@info.com", "netter Ort für lange Wochenenden")));
+            log.info("Preloading " + repository.save(new Venue("café tennis", "zumtennis@info.com", "nettes café für nette Abende")));
+        };
+    }
+
+}
