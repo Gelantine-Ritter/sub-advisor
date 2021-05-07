385dfef548a066a2d0ceeef9ecf944ab108d63c9
Matti Henning <s80116@beuth-hochschule.de>
2021-05-07 22:47:02 +0200

    MP-88 [MOD] return-type of createVenue and updateVenueById from void to Venue to get Entity in response-body

--- backend/src/main/java/com/subadvisor/venue/VenueController.java
+++ backend/src/main/java/com/subadvisor/venue/VenueController.java
@@ -2,8 +2,8 @@ package com.subadvisor.venue;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 public class VenueController {
@@ -12,27 +12,27 @@ public class VenueController {
     VenueService venueService;
 
     @GetMapping("/venues")
-    public List<Venue> getAllVenues(){
+    public List<Venue> getAllVenues() {
         return venueService.getAllVenues();
     }
 
     @PostMapping(path = "/venues")
-    public void createVenue (@RequestBody Venue venue) {
-        venueService.createVenue(venue);
+    public Venue createVenue(@RequestBody Venue venue) {
+        return venueService.createVenue(venue);
     }
 
     @GetMapping("/venues/{id}")
-    public Venue getVenueById(@PathVariable(value="id") Long venueId){
+    public Venue getVenueById(@PathVariable(value = "id") Long venueId) {
         return venueService.getVenueById(venueId);
     }
 
     @DeleteMapping("/venues/{id}")
-    public void deleteVenueById(@PathVariable(value="id") Long venueId){
+    public void deleteVenueById(@PathVariable(value = "id") Long venueId) {
         venueService.deleteVenueById(venueId);
     }
 
     @PutMapping("/venues/{id}")
-    public void updateVenueById(@RequestBody Venue newVenue, @PathVariable Long id) {
-        venueService.updateVenueById(newVenue, id);
+    public Venue updateVenueById(@RequestBody Venue newVenue, @PathVariable Long id) {
+        return venueService.updateVenueById(newVenue, id);
     }
 }
