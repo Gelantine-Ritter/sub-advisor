5e124f222f54c0e229c9f74952f9dccf78ea8c8b
Matti Henning <s80116@beuth-hochschule.de>
2021-05-07 14:42:31 +0200

    MP-86 [ADD] initial VenueIT.java and Test if VenueController is present.

---
+++ backend/src/test/java/com/subadvisor/venue/VenueIT.java
@@ -0,0 +1,43 @@
+package com.subadvisor.venue;
+
+import com.subadvisor.BackendApplication;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mock.web.MockServletContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.util.Assert;
+import org.springframework.web.context.WebApplicationContext;
+
+import javax.servlet.ServletContext;
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = {BackendApplication.class})
+@WebAppConfiguration
+public class VenueIT {
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    private MockMvc mockMvc;
+
+    @BeforeEach
+    public void setup() throws Exception {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.webApplicationContext).build();
+    }
+
+    @Test
+    public void givenWac_whenServletContext_thenItProvidesGreetController() {
+        ServletContext servletContext = webApplicationContext.getServletContext();
+
+        Assertions.assertNotNull(servletContext);
+        Assertions.assertTrue(servletContext instanceof MockServletContext);
+        Assertions.assertNotNull(webApplicationContext.getBean("venueController"));
+    }
+}
