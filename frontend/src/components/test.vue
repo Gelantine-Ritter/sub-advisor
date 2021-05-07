af544de1566b62af448055263ed1f91af8632727
Anna R <s72775@beuth-hochschule.de>
2021-05-07 13:29:55 +0200

    [ADD] added project structure for frontend

--- frontend/src/components/test.vue
+++ frontend/src/components/test.vue
@@ -1,13 +1,23 @@
-<template></template>
+<template>
+  <div>
+    <ul>
+      <li v-for="test in tests" :key="test.index">{{ test }}</li>
+    </ul>
+  </div>
+</template>
 
 <script>
 export default {
   data() {
     return {
-      firstName: 'Anna',
+      tests: ['hallo', 'blubbl', 'okcool'],
     }
   },
 }
 </script>
 
-<style></style>
+<style>
+ul {
+  color: rgb(255, 0, 34);
+}
+</style>
