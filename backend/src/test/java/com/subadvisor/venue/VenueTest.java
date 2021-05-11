3b88382ba583a74c3291dd3ea4b3fa3804263cc5
Kilian Klammt <s73072@beuth-hochschule.de>
2021-05-11 16:42:20 +0200

    MP-88 [ADD] add local compiling unit tests for VENUE

---
+++ backend/src/test/java/com/subadvisor/venue/VenueTest.java
@@ -0,0 +1,54 @@
+package com.subadvisor.venue;
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class VenueTest {
+
+
+    private static Venue venue;
+
+    @Before
+    public void setUp () {
+        venue = new Venue("Berghain", "berghain@steil.de", "Tolle Hütte, viel Freude.");
+    }
+
+    @Test
+    public void testGetVenueName() {
+        assertEquals(venue.getName(), "Berghain");
+    }
+
+    @Test
+    public void testGetVenueEmail() {
+        assertEquals("berghain@steil.de", venue.getEmail());
+    }
+
+    @Test
+    public void testGetVenueInfo() {
+        assertEquals(venue.getInfo(), "Tolle Hütte, viel Freude.");
+    }
+
+    @Test
+    public void testSetVenueName() {
+        String name = "AboutBlank";
+        venue.setName(name);
+        assertEquals(name, venue.getName());
+    }
+
+    @Test
+    public void testSetVenueEmail() {
+        String email = "andereMail@berghain.com";
+        venue.setEmail(email);
+        assertEquals(email, venue.getEmail());
+    }
+
+    @Test
+    public void testSetVenueInfo() {
+        String info = "War mal hier, war nicht so...";
+        venue.setInfo(info);
+        assertEquals(info, venue.getInfo());
+    }
+}
+
+
