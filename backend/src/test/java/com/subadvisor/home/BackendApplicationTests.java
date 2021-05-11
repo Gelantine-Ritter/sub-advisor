6bada7c1930686a2c99e20c1fcb12c5ec048b714
Kilian Klammt <s73072@beuth-hochschule.de>
2021-05-11 15:33:46 +0200

    MP-88 [MOD] add first working Test

--- backend/src/test/java/com/subadvisor/home/BackendApplicationTests.java
+++ backend/src/test/java/com/subadvisor/home/BackendApplicationTests.java
@@ -3,11 +3,16 @@ package com.subadvisor.home;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
 class BackendApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void testHomeController(){
+		TestController testController = new TestController();
+		String response = testController.greeting();
+		assertEquals(response, "Hey there, this is the backend-service for sub-advisor!");
 	}
 
 }
