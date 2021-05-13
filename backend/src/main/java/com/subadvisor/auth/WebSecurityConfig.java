ab1cb6ba57d1fb578177078743138bc09c6da5e9
Matti Henning <s80116@beuth-hochschule.de>
2021-05-13 10:15:52 +0200

    MP-69 [REF] WebSecurityConfig and changed sequenze of matchers, renamed customUserService to userService

--- backend/src/main/java/com/subadvisor/auth/WebSecurityConfig.java
+++ backend/src/main/java/com/subadvisor/auth/WebSecurityConfig.java
@@ -1,8 +1,14 @@
 package com.subadvisor.auth;
 
 
+import com.subadvisor.user.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -13,6 +19,8 @@ import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import javax.servlet.http.HttpServletResponse;
+
 
 /**
  * This class is required to customize the Spring Security layer
@@ -20,18 +28,22 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
  * @author Matti Henning
  */
 
-@EnableWebSecurity
+@Configuration
+@EnableWebSecurity(debug = true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
+
+    Logger logger = LoggerFactory.getLogger("Logger");
+
     @Autowired
-    private CustomUserDetailsService customUserDetailsService;
+    private UserService userService;
 
     @Autowired
     private JwtRequestFilter jwtRequestFilter;
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(customUserDetailsService);
+        auth.userDetailsService(userService);
     }
 
     @Override
@@ -41,21 +53,47 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
     @Override
-    protected void configure(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity
-                .csrf()
-                .disable()
-                .authorizeRequests()
-                .antMatchers("/authenticate")
-                .permitAll()
-                .anyRequest()
-                .authenticated()
-                .and().exceptionHandling()
-                .and().sessionManagement()
-                // important, otherwise JWT is not used
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+    protected void configure(HttpSecurity http) throws Exception {
 
-        httpSecurity.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
+        http.headers().frameOptions().disable().and().cors().and().csrf().disable();
+
+        // Set session management to stateless
+        http = http
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and();
+
+        // Set unauthorized requests exception handler
+        http = http
+                .exceptionHandling()
+                .authenticationEntryPoint(
+                        (request, response, ex) -> {
+                            logger.error("Unauthorized request - {}", ex.getMessage());
+                            response.sendError(HttpServletResponse.SC_UNAUTHORIZED, ex.getMessage());
+                        }
+                )
+                .and();
+
+
+        http
+                .authorizeRequests()
+                // public endpoints
+                .antMatchers("/authenticate").permitAll()
+                .antMatchers("/").permitAll()
+                .anyRequest().authenticated();
+
+
+        http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
+
+        /*
+        @Override
+        protected void configure(HttpSecurity httpSecurity) throws Exception {
+            httpSecurity.headers().frameOptions().disable().and().cors().and().csrf().disable();
+            httpSecurity.addFilterBefore(this.jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
+        }
+
+         */
+
     }
 
     @Bean
