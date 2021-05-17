a429243432052900314a67473c58a65bd3f9e758
Matti Henning <s80116@beuth-hochschule.de>
2021-05-17 21:35:24 +0200

    MP-78 [ADD] simple-progress-webpack-plugin as new dependency to frontend and use in vue.config.js to minimize serverlog.

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,3 +1,15 @@
+const SimpleProgressWebpackPlugin = require('simple-progress-webpack-plugin')
+
 module.exports = {
+  configureWebpack: {
+    plugins: [
+      new SimpleProgressWebpackPlugin({
+        format: 'minimal'
+      })
+    ]
+  },
+  devServer: {
+    progress: false,
+  },
   transpileDependencies: ['vuetify'],
 }
