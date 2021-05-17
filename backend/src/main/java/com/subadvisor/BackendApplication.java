ac73fe671a106da1c38c3f91ef49bbd1058e7eea
Matti Henning <s80116@beuth-hochschule.de>
2021-05-17 21:33:01 +0200

    MP-78 [ADD] new application.properties-profiles to project (default and dev) and add log to applicationRunner()-bean to log active profile.

--- backend/src/main/java/com/subadvisor/BackendApplication.java
+++ backend/src/main/java/com/subadvisor/BackendApplication.java
@@ -1,15 +1,32 @@
 package com.subadvisor;
 
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
 
-
+@Log4j2
 @SpringBootApplication
 public class BackendApplication {
 
 	public static void main(String... args) {
 		SpringApplication.run(BackendApplication.class, args);
 	}
+
+	@Bean
+	ApplicationRunner applicationRunner(Environment env ,
+										// $HOME is system-env-varibale. Will print something like /Users/matti
+										@Value("${spring_profiles_active}") String profile
+										) {
+		return args -> {
+			// using @Value is better than env
+			log.info("Welcome from Application-Runner. Your active profile is " + profile);
+		};
+	}
+
 
 }
 
