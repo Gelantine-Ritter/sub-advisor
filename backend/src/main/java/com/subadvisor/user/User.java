213b67af80c2e0b3be83c45d66eb5ed10846f46d
Matti Henning <s80116@beuth-hochschule.de>
2021-05-17 21:39:38 +0200

    MP-78 [ADD] getPassword() and getUsername() to User since it implements UserDetails-Interface. Also modiefied with @Table(users) to specify postgresTable since user is reserved keyword in postgres.

--- backend/src/main/java/com/subadvisor/user/User.java
+++ backend/src/main/java/com/subadvisor/user/User.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.security.core.GrantedAuthority;
@@ -12,16 +13,19 @@ import org.springframework.security.core.userdetails.UserDetails;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.Table;
 import java.io.Serializable;
 import java.time.LocalDateTime;
 import java.util.Collection;
 
 
 @Entity
+@Table(name = "`users`")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
+@Accessors(fluent = true, chain = true)
 public class User implements UserDetails, Serializable {
 
     @Id
@@ -39,6 +43,16 @@ public class User implements UserDetails, Serializable {
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
     }
 
     @Override
