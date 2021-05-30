e8c71f46c90299282a0d4a1af6d9eebf544d7155
Kilian Klammt <s73072@beuth-hochschule.de>
2021-05-30 17:10:00 +0200

    MP-102 [ADD] initial it-tests venue registration

--- backend/src/test/java/com/subadvisor/api/auth/RegistrationIT.java
+++ backend/src/test/java/com/subadvisor/api/auth/RegistrationIT.java
@@ -2,6 +2,7 @@ package com.subadvisor.api.auth;
 
 import com.subadvisor.api.Driver;
 import com.subadvisor.api.venue.Venue;
+import com.subadvisor.operators.LoginOperator;
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.http.MediaType;
@@ -23,10 +24,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class RegistrationIT extends Driver {
-/*
+
     private static Venue VENUE;
-    private static Venue OTHER_VENUE;
-    private static Venue VENUE_UPDATED;
 
     private static final String USER_NAME_VENUE = "about-party";
     private static final String PASSWORD_VENUE = "roterosen161";
@@ -40,12 +39,12 @@ public class RegistrationIT extends Driver {
     private static final String INFO_OTHER_VENUE = "gemütliche Kneipe mit schönem Keller";
     private static final String EMAIL_OTHER_VENUE = "tennis@schlaeger.info";
 
+    private static String TOKEN_VENUE;
     private Driver DRIVER = driver();
 
     @BeforeAll
     void setUp() throws Exception {
 
-        // FIXME: Since registration is not implemtneted yet, this is roundabout
         VENUE = Venue.builder()
                 .username(USER_NAME_VENUE)
                 .password(PASSWORD_VENUE)
@@ -53,23 +52,6 @@ public class RegistrationIT extends Driver {
                 .email(EMAIL_VENUE)
                 .info(INFO_VENUE)
                 .build();
-
-        OTHER_VENUE = Venue.builder()
-                .username(USER_NAME_OTHER_VENUE)
-                .password(PASSWORD_OTHER_VENUE)
-                .name(NAME_OTHER_VENUE)
-                .email(EMAIL_OTHER_VENUE)
-                .info(INFO_OTHER_VENUE)
-                .build();
-
-        VENUE_UPDATED = Venue.builder()
-                .username(USER_NAME_VENUE)
-                .password(PASSWORD_VENUE)
-                .name(NAME_VENUE + "_update")
-                .email(EMAIL_VENUE + "_update")
-                .info(INFO_VENUE + "_update")
-                .build();
-
     }
 
     @Test
@@ -85,6 +67,70 @@ public class RegistrationIT extends Driver {
                                 ))
 
                 )
+                .andDo(print())
+                .andExpect(
+                        matchAll(
+                                status().isOk(),
+                                jsonPath("$.username").value(VENUE.username()),
+                                jsonPath("$.name").value(VENUE.name()),
+                                jsonPath("$.info").value(VENUE.info()),
+                                jsonPath("$.email").value(VENUE.email()),
+                                jsonPath("$.password").doesNotExist(),
+                                jsonPath("$.created").exists(),
+                                jsonPath("$.modifiedAt").exists(),
+                                status().isOk()
+                        )
+                )
+                .andDo(print())
+                .andReturn()
+                .getResponse();
+    }
+
+    @Test
+    @Order(2)
+    void logInNewRegistratedVenue() throws Exception {
+        TOKEN_VENUE =
+                new LoginOperator(DRIVER)
+                        .login(USER_NAME_VENUE, PASSWORD_VENUE)
+                        .token();
+    }
+
+    /*
+
+    @Test
+    @Order(3)
+    void venueIsPresentInDataBase() throws Exception{
+        DRIVER.mockMvc()
+                .perform(
+                        get("/venues/" + VENUE.id())
+                )
+                .andExpect(
+                        matchAll(
+                                status().isOk(),
+                                jsonPath("$.username").doesNotExist(),
+                                jsonPath("$.id").doesNotExist(),
+                                jsonPath("$.name").value(VENUE.name()),
+                                jsonPath("$.info").value(VENUE.info()),
+                                jsonPath("$.email").value(VENUE.email()),
+                                jsonPath("$.password").doesNotExist(),
+                                jsonPath("$.created").doesNotExist(),
+                                jsonPath("$.modifiedAt").doesNotExist(),
+                                status().isOk()
+                        )
+                )
+                .andDo(print())
+                .andReturn()
+                .getResponse();
+    }
+
+    @Test
+    @Order(4)
+    void venueCanGetPrivateProfileWhenLoggedIn() throws Exception{
+        DRIVER.mockMvc()
+                .perform(
+                        get("/venues/" + VENUE.id())
+                                .header("authorization", "Bearer " + TOKEN_VENUE)
+                )
                 .andExpect(
                         matchAll(
                                 status().isOk(),
@@ -93,8 +139,8 @@ public class RegistrationIT extends Driver {
                                 jsonPath("$.name").value(VENUE.name()),
                                 jsonPath("$.info").value(VENUE.info()),
                                 jsonPath("$.email").value(VENUE.email()),
-                                jsonPath("$.password").value(VENUE.password()),
-                                jsonPath("$.created").value(VENUE.created()),
+                                jsonPath("$.password").doesNotExist(),
+                                jsonPath("$.created").doesNotExist(),
                                 jsonPath("$.modifiedAt").doesNotExist(),
                                 status().isOk()
                         )
@@ -102,7 +148,7 @@ public class RegistrationIT extends Driver {
                 .andDo(print())
                 .andReturn()
                 .getResponse();
-
     }
-            */
+
+    */
 }
