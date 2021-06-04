5c086807b28d59b6c677305857b71cd77c8e9735
Matti Henning <s80116@beuth-hochschule.de>
2021-06-04 13:44:11 +0200

    MP-261 [REF] createEvent ind EventService to call new EventMapper to map from dto to entity

--- backend/src/main/java/com/subadvisor/api/event/EventService.java
+++ backend/src/main/java/com/subadvisor/api/event/EventService.java
@@ -1,33 +1,49 @@
 package com.subadvisor.api.event;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.subadvisor.DataAccess;
+import com.subadvisor.api.event.dto.EventCreateDto;
+import com.subadvisor.api.venue.VenueRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+
 @Service
-public class EventService implements IEventService {
+public class EventService extends DataAccess implements IEventService {
 
     @Autowired
-    EventRepository repository;
+    EventRepository eventRepo;
+
+    @Autowired
+    VenueRepository venueRepo;
+
+    @Autowired
+    DataAccess DATA;
 
     @Override
-    public List<Event> getAllEvents(){
-        return repository.findAll();
-    }
-    @Override
-    public void createEvent(Event event) {
-        repository.save(event);
+    public List<Event> getAllEvents() {
+        return eventRepo.findAll();
     }
 
     @Override
-    public Optional<Event> getEventById(Long eventId){
-        return repository.findById(eventId);
+    public Event createEvent(EventCreateDto dto) {
+
+        return DATA.eventRepository().save(
+                new EventMapper(DATA).mapToEventEntity(dto)
+        );
+        //objectMapper.convertValue(eventCreateDto, Event.class)
+    }
+
+    @Override
+    public Optional<Event> getEventById(Long eventId) {
+        return eventRepo.findById(eventId);
     }
 
     @Override
     public void deleteEventById(Long eventId) {
-        repository.deleteById(eventId);
+        eventRepo.deleteById(eventId);
     }
 }
--- backend/src/main/java/com/subadvisor/api/event/IEventService.java
+++ backend/src/main/java/com/subadvisor/api/event/IEventService.java
@@ -1,11 +1,13 @@
 package com.subadvisor.api.event;
 
+import com.subadvisor.api.event.dto.EventCreateDto;
+
 import java.util.List;
 import java.util.Optional;
 
 public interface IEventService {
     List<Event> getAllEvents();
-    void createEvent(Event event);
+    Event createEvent(EventCreateDto event);
     Optional<Event> getEventById(Long eventId);
     void deleteEventById(Long eventId);
 }
