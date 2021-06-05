0998c205b19ec2c535465d3521505d78869f2f0c
Matti Henning <s80116@beuth-hochschule.de>
2021-06-05 13:32:56 +0200

    MP-247 [ADD] add Spring-validation-dependency to pom.xml

--- backend/src/main/java/com/subadvisor/api/event/EventController.java
+++ backend/src/main/java/com/subadvisor/api/event/EventController.java
@@ -5,6 +5,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
@@ -14,13 +16,13 @@ public class EventController {
     EventService eventService;
 
     @GetMapping("/events/")
-    public List<Event> getAllEvents() {
-
-        return eventService.getAllEvents();
+    public List<Event> getAllEvents(@RequestParam("venue") String venueName) {
+        return venueName != null ? eventService.getEventsByVenue(venueName) :
+                eventService.getAllEvents();
     }
 
     @PostMapping("/events/")
-    public ResponseEntity<?> createEvent(@RequestBody EventCreateDto newEvent) {
+    public ResponseEntity<?> createEvent(@Valid @RequestBody EventCreateDto newEvent) {
         return new ResponseEntity<>(
                 eventService.createEvent(newEvent),
                 HttpStatus.OK
