0ea0a0d244b413c880ee2a973483be36cc982648
Matti Henning <s80116@beuth-hochschule.de>
2021-06-06 13:08:23 +0200

    MP-262 [ADD] EventUpdateDto

---
+++ backend/src/main/java/com/subadvisor/api/event/dto/EventUpdateDto.java
@@ -0,0 +1,27 @@
+package com.subadvisor.api.event.dto;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.*;
+import lombok.experimental.Accessors;
+import javax.validation.constraints.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Accessors(fluent = true)
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
+public class EventUpdateDto {
+
+    @NotNull(message = "Please provide a title")
+    @NotEmpty(message = "title should not be empty")
+    private String title;
+    private String info;
+    private String[] artists;
+    private String price;
+    private String eventStart;
+    private String eventEnd;
+    private byte[] pic;
+}
