ca0516d566c2c68b91f00ab8159408324067b142
Anna R <s72775@beuth-hochschule.de>
2021-06-06 16:48:43 +0200

    [ADD] profile page for logged in venue + authenticated routes + modal

--- frontend/src/views/VenueProfile.vue
+++ frontend/src/views/VenueProfile.vue
@@ -1,11 +1,57 @@
 <template>
   <v-container>
-    <h1>Potected with auth</h1>
+    <div class="rounded-xl mycontainer" fluid>
+      <h4>GENERAL <v-icon dense color="grey">far fa-edit</v-icon></h4>
+
+      <v-list-item two-line>
+        <v-list-item-content>
+          <v-list-item-subtitle> PLACE </v-list-item-subtitle>
+          <v-list-item-title> {{ auth.user.name }} </v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+      <v-list-item two-line>
+        <v-list-item-content>
+          <v-list-item-subtitle>USERNAME</v-list-item-subtitle>
+          <v-list-item-title> ..... </v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+      <v-list-item two-line>
+        <v-list-item-content>
+          <v-list-item-subtitle>MAIL</v-list-item-subtitle>
+          <v-list-item-title> {{ auth.user.email }} </v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+      <v-divider></v-divider>
+
+      <v-list-item two-line>
+        <v-list-item-content>
+          <v-list-item-subtitle>DESCRIPTION</v-list-item-subtitle>
+          <v-list-item-title> {{ auth.user.info }} </v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+      <span>edit</span>
+    </div>
   </v-container>
 </template>
 
 <script>
-export default {}
+import { mapState } from 'vuex'
+export default {
+  computed: {
+    ...mapState(['auth']),
+  },
+}
 </script>
 
-<style></style>
+<style>
+.mycontainer {
+  border: solid 2px black;
+  width: auto;
+  margin-top: 5vw;
+  margin-left: 15vw;
+  margin-right: 15vw;
+  margin-bottom: 5vw;
+  background: white;
+  padding: 5vw;
+}
+</style>
