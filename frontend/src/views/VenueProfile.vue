d65ebf2191a113e6a4f28329e417d6a48212e244
Anna R <s72775@beuth-hochschule.de>
2021-06-09 15:41:21 +0200

    MP-198 [ADD] layout for profile

--- frontend/src/views/VenueProfile.vue
+++ frontend/src/views/VenueProfile.vue
@@ -1,14 +1,32 @@
 <template>
   <v-container>
     <div class="rounded-xl mycontainer" fluid>
-      <h4>GENERAL <v-icon dense color="grey">far fa-edit</v-icon></h4>
-
-      <v-list-item two-line>
-        <v-list-item-content>
-          <v-list-item-subtitle> PLACE </v-list-item-subtitle>
-          <v-list-item-title> {{ auth.user.name }} </v-list-item-title>
-        </v-list-item-content>
-      </v-list-item>
+      <v-card center class="rounded-xl ma-7" :style="styleObject">
+        <v-responsive>
+          <v-img
+            class="ma-5 border border-dark"
+            height="250"
+            src="../../public/venue2.png"
+            alt=""
+          />
+        </v-responsive>
+      </v-card>
+      <div>
+        <v-card class="d-flex justify-space-between mb-6" flat tile>
+          <v-card class="pa-2" flat tile>
+            <h4>GENERAL</h4>
+          </v-card>
+          <v-card class="pa-2" flat tile>
+            <h4>
+              <v-btn icon class="ml-5">
+                <v-icon class="text-right" dense color="grey"
+                  >far fa-edit</v-icon
+                >
+              </v-btn>
+            </h4>
+          </v-card>
+        </v-card>
+      </div>
       <v-list-item two-line>
         <v-list-item-content>
           <v-list-item-subtitle>USERNAME</v-list-item-subtitle>
@@ -21,7 +39,36 @@
           <v-list-item-title> {{ auth.user.email }} </v-list-item-title>
         </v-list-item-content>
       </v-list-item>
+      <v-list-item two-line>
+        <v-list-item-content>
+          <v-list-item-subtitle> PASSWORD </v-list-item-subtitle>
+          <v-list-item-title> ... </v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
       <v-divider></v-divider>
+      <div>
+        <v-card class="d-flex justify-space-between mb-6" flat tile>
+          <v-card class="pa-2" flat tile>
+            <h4>YOUR PLACE</h4>
+          </v-card>
+          <v-card class="pa-2" flat tile>
+            <h4>
+              <v-btn icon class="ml-5">
+                <v-icon class="text-right" dense color="grey"
+                  >far fa-edit</v-icon
+                >
+              </v-btn>
+            </h4>
+          </v-card>
+        </v-card>
+      </div>
+
+      <v-list-item two-line>
+        <v-list-item-content>
+          <v-list-item-subtitle> PLACE </v-list-item-subtitle>
+          <v-list-item-title> {{ auth.user.name }} </v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
 
       <v-list-item two-line>
         <v-list-item-content>
@@ -29,7 +76,19 @@
           <v-list-item-title> {{ auth.user.info }} </v-list-item-title>
         </v-list-item-content>
       </v-list-item>
-      <span>edit</span>
+
+      <v-list-item two-line>
+        <v-list-item-content>
+          <v-list-item-subtitle>OPENING HOURS</v-list-item-subtitle>
+          <v-list-item-title> ... </v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+      <v-list-item two-line>
+        <v-list-item-content>
+          <v-list-item-subtitle>ADDRESS</v-list-item-subtitle>
+          <v-list-item-title> ... </v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
     </div>
   </v-container>
 </template>
@@ -37,6 +96,11 @@
 <script>
 import { mapState } from 'vuex'
 export default {
+  data() {
+    return {
+      styleObject: { border: '2px solid #cafb03' },
+    }
+  },
   computed: {
     ...mapState(['auth']),
   },
