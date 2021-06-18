dc489857d582c6c86bc32941cf0c4c44b28b227a
Matti Henning <s80116@beuth-hochschule.de>
2021-06-18 12:17:41 +0200

    MP-329 [ADD] new dummy-data to LoadDatabase-Configuration

--- backend/src/main/java/com/subadvisor/LoadDatabase.java
+++ backend/src/main/java/com/subadvisor/LoadDatabase.java
@@ -23,8 +23,7 @@ class LoadDatabase {
     CommandLineRunner initDatabase(VenueRepository venueRepository, MemberRepository memberRepository, EventRepository eventRepository) {
         return args -> {
 
-            Venue trudeRuth = venueRepository.save(
-                    Venue.builder()
+            Venue trudeRuth = venueRepository.save(Venue.builder()
                             .username("trude-ruth")
                             .password("rudiruth")
                             .name("Trude Ruth")
@@ -53,14 +52,48 @@ class LoadDatabase {
                             .build()
             );
 
+            Venue hochbeetGarten = venueRepository.save(
+                    Venue.builder()
+                            .username("hochhinaus")
+                            .password("hochhoch")
+                            .name("Hochbeet Wedding")
+                            .email("hochbeet@info.com")
+                            .info("Ein schöner Garten direkt am Leopoldplatz. Hier kann man entspannen, on der Sonne sitzen" +
+                                    "und leckere Salate direkt aus dem eigenen Garten essen.")
+                            .mobile("030-198754")
+                            .hours(
+                                    Map.of("monday", "closed",
+                                            "tuesday", "10:00 - 18:00",
+                                            "wednesday", "10:00 - 18:00",
+                                            "thursday", "10:00 - 18:00",
+                                            "friday", "10:00 - 18:00",
+                                            "saturday", "10:00 - 18:00",
+                                            "sunday", "10:00 - 18:00"
+                                    )
+                            )
+                            .address(
+                                    Map.of(
+                                            "street", "Leopoldplatz",
+                                            "number", "1",
+                                            "city", "Berlin",
+                                            "plz", "13347"
+                                    )
+                            )
+                            .website("https://hochbeet-wedding.com")
+                            .build()
+            );
+
 
             // Load Venues
             log.info("Preloading " + trudeRuth);
+            log.info("Preloading " + hochbeetGarten);
 
             Event eventTrudeRuth01 = eventRepository.save(
                     Event.builder()
                     .title("Tischtennis am Mittwoch")
                     .venue(trudeRuth)
+                    .eventStart(LocalDateTime.parse("2021-09-04T17:00:00"))
+                    .eventEnd(LocalDateTime.parse("2021-09-04T19:00"))
                     .info("nettes indoor-tischtennis im Hinterraum")
                     .artists(Set.of("Timo Boll"))
                     .price(10)
@@ -73,6 +106,34 @@ class LoadDatabase {
                             .venue(trudeRuth)
                             .info("schön Salsa tanzen am Dienstag")
                             .artists(Set.of("DJ Salsa", "MC Salsa"))
+                            .eventStart(LocalDateTime.parse("2021-09-07T17:00:00"))
+                            .eventEnd(LocalDateTime.parse("2021-09-07T19:00"))
+                            .price(0)
+                            .build()
+            );
+
+            Event hochbeet01 = eventRepository.save(
+                    Event.builder()
+                            .title("Jede:r hat einen grünen Daumen")
+                            .venue(hochbeetGarten)
+                            .eventStart(LocalDateTime.parse("2021-08-07T17:00:00"))
+                            .eventEnd(LocalDateTime.parse("2021-08-07T19:00"))
+                            .info("alle haben mal klein angefangen! Unsere profi-gaertner*innen Samuel und Nayla zeigen wie ihr" +
+                                    "auch in eurer Wohnung schoene Kartoffeln anbauen koennt")
+                            .artists(Set.of("Nalya Hammerschlag", "Samuel Rosenwald"))
+                            .price(5)
+                            .build()
+            );
+
+            Event hochbeet02 = eventRepository.save(
+                    Event.builder()
+                            .title("Salatchips selber machen")
+                            .venue(hochbeetGarten)
+                            .info("In Unserer Offenen Kueche macht Haribert immer einmal in der Woche Salatchips. " +
+                                    "Kommt gerne rum")
+                            .eventStart(LocalDateTime.parse("2021-06-07T17:00:00"))
+                            .eventEnd(LocalDateTime.parse("2021-06-07T19:00"))
+                            .artists(Set.of("Chief of cuisine - Haribert"))
                             .price(0)
                             .build()
             );
@@ -83,6 +144,10 @@ class LoadDatabase {
             log.info("Preloading " + eventTrudeRuth01);
 
             log.info("Preloading " + eventTrudeRuth02);
+
+            log.info("Preloading " + hochbeet01);
+
+            log.info("Preloading " + hochbeet02);
 
             // Load TestUser
             log.info("Preloading " + memberRepository.save(
