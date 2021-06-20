9a481b5f187accba2ff0db8e376e00ab5687cb3e
Anna R <s72775@beuth-hochschule.de>
2021-06-20 17:48:18 +0200

    MP-322 [ADD] EmailJs for contact form

--- frontend/src/views/ContactPage.vue
+++ frontend/src/views/ContactPage.vue
@@ -1,47 +1,60 @@
 <template>
- 
   <v-container class="rounded-xl mycontainer" fluid>
-       <h1 class="h1Style text-center display-3 font-weight-medium">CONTACT US</h1>
+    <h1 class="h1Style text-center display-3 font-weight-medium">CONTACT US</h1>
     <div>
-      <v-form>
+      <v-form @submit.prevent="sendEmail">
         <v-row>
           <v-col cols="12" sm="6">
-            <v-text-field 
-              v-model="name"
-              label="NAME"
-          ></v-text-field>
+            <v-text-field v-model="from_name" label="NAME"></v-text-field>
           </v-col>
           <v-col cols="12" sm="6">
-            <v-text-field 
-              v-model="mail"
-              label="MAIL"
-            ></v-text-field>
+            <v-text-field v-model="reply_to" label="MAIL"></v-text-field>
           </v-col>
           <v-col cols="12">
-            <v-textarea
-              v-model="message"
-            >
+            <v-textarea v-model="message">
               <template v-slot:label>
                 <div>MESSAGE</div>
               </template>
             </v-textarea>
           </v-col>
         </v-row>
-         <v-btn
-                width="100%"
-                flat
-                color="rounded-pill black white--text"
-                >SUBMIT
-              </v-btn>
+        <input type="submit" value="Send" />
       </v-form>
     </div>
   </v-container>
 </template>
 
 <script>
+import emailjs from 'emailjs-com'
+
 export default {
+  data() {
+    return {
+      from_name: '',
+      mail: '',
+      message: '',
+    }
+  },
 
-
+  methods: {
+    sendEmail(e) {
+      emailjs
+        .sendForm(
+          'service_3jepy7a',
+          'template_69fu98a',
+          e.target,
+          'user_i2XDe74FgVZ1VEc62xset'
+        )
+        .then(
+          (result) => {
+            console.log('SUCCESS!', result.status, result.text)
+          },
+          (error) => {
+            console.log('FAILED...', error)
+          }
+        )
+    },
+  },
 }
 </script>
 
